1830a69c4bdffbabf83188090dc3fe4f95e69951
aniket91 <aniketthakur91@gmail.com>
2018-09-14 19:50:55 +0530

    Adding CICD

---
+++ scripts/node-lambdas.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Started - lambda $1"
+
+if [ "$1" = "build" ]; then
+  cd $HOME_DIR/lambdas/fetch-s3-data && npm install
+
+elif [ "$1" = "package" ]; then
+  cd $HOME_DIR/lambdas/fetch-s3-data && zip -9rq fetch-s3-data.zip .
+
+fi
+echo "Ended - lambda $1"
