c18606bc3297fc9525cd74bfd783665b3353f653
aniket91 <aniketthakur91@gmail.com>
2018-09-22 12:25:06 +0530

    Adding cleanup lambda

--- scripts/node-lambdas.sh
+++ scripts/node-lambdas.sh
@@ -3,9 +3,11 @@ echo "Started - lambda $1"
 
 if [ "$1" = "build" ]; then
   cd $HOME_DIR/lambdas/fetch-s3-data && npm install
+  cd $HOME_DIR/lambdas/clean-s3-data && npm install
 
 elif [ "$1" = "package" ]; then
   cd $HOME_DIR/lambdas/fetch-s3-data && zip -9rq fetch-s3-data.zip .
+  cd $HOME_DIR/lambdas/clean-s3-data && zip -9rq clean-s3-data.zip .
 
 fi
 echo "Ended - lambda $1"
